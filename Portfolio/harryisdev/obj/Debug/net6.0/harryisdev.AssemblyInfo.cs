2be77f0802d8451ce51e27dcaeb32e5b21a1a9c4
Harpal <harryharpalsingh@gmail.com>
2024-09-17 17:52:53 +0530

    17 September 2024 5:52PM

--- Portfolio/harryisdev/obj/Debug/net6.0/harryisdev.AssemblyInfo.cs
+++ Portfolio/harryisdev/obj/Debug/net6.0/harryisdev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a699664b9eee66a852bc389dc417b833c0865ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a669f5110d069cabe8e987de55628b6b56e0e7a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
