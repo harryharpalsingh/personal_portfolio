8f4d47f54ca882cdfb55deebb0ba9f3cc09f6435
Harpal <harryharpalsingh@gmail.com>
2024-10-08 18:31:25 +0530

    Added Dashboard using Areas in .Net Core 08-October-2024

--- Portfolio/harryisdev/obj/Release/net6.0/harryisdev.AssemblyInfo.cs
+++ Portfolio/harryisdev/obj/Release/net6.0/harryisdev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349128d976a79920e066237dab1dfaeeaf8af84c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602e09fb57d818509a3903f6cc296e17ed31afb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
