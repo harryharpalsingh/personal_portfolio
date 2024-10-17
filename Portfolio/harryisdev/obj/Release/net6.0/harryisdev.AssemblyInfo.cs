425019a5e756021d3bfa8fdcbec6320fc5d013c2
Harpal <harryharpalsingh@gmail.com>
2024-10-17 18:29:41 +0530

    added gitignore

--- Portfolio/harryisdev/obj/Release/net6.0/harryisdev.AssemblyInfo.cs
+++ Portfolio/harryisdev/obj/Release/net6.0/harryisdev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c371b39af0112bfbd838698025ccbfbda13d2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e353a00e991f279332db864804465ba5130dfb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("harryisdev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
